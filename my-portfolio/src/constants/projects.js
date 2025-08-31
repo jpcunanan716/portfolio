221804d42baa7b91c89ce8a97e1e811bed4b57bc
jpcunanan716 <jpcunanan716@gmail.com>
2025-08-31 12:04:07 +0800

    added new project in constants

--- my-portfolio/src/constants/projects.js
+++ my-portfolio/src/constants/projects.js
@@ -29,6 +29,25 @@ import btb11 from "/src/assets/images/btbimages/btb11.jpg";
 import btb12 from "/src/assets/images/btbimages/btb12.jpg";
 import btb13 from "/src/assets/images/btbimages/btb13.jpg";
 import btb14 from "/src/assets/images/btbimages/btb14.jpg";
+import centurycurated1 from "/src/assets/images/centurycuratedimages/centurycurated1.jpg";
+import centurycurated2 from "/src/assets/images/centurycuratedimages/centurycurated2.jpg";
+import centurycurated3 from "/src/assets/images/centurycuratedimages/centurycurated3.jpg";
+import centurycurated4 from "/src/assets/images/centurycuratedimages/centurycurated4.jpg";
+import centurycurated5 from "/src/assets/images/centurycuratedimages/centurycurated5.jpg";
+import centurycurated6 from "/src/assets/images/centurycuratedimages/centurycurated6.jpg";
+import centurycurated7 from "/src/assets/images/centurycuratedimages/centurycurated7.jpg";
+import centurycurated8 from "/src/assets/images/centurycuratedimages/centurycurated8.jpg";
+import centurycurated9 from "/src/assets/images/centurycuratedimages/centurycurated9.jpg";
+import centurycurated10 from "/src/assets/images/centurycuratedimages/centurycurated10.jpg";
+import centurycurated11 from "/src/assets/images/centurycuratedimages/centurycurated11.jpg";
+import centurycurated12 from "/src/assets/images/centurycuratedimages/centurycurated12.jpg";
+import centurycurated13 from "/src/assets/images/centurycuratedimages/centurycurated13.jpg";
+import centurycurated14 from "/src/assets/images/centurycuratedimages/centurycurated14.jpg";
+import centurycurated15 from "/src/assets/images/centurycuratedimages/centurycurated15.jpg";
+import centurycurated16 from "/src/assets/images/centurycuratedimages/centurycurated16.jpg";
+import centurycurated17 from "/src/assets/images/centurycuratedimages/centurycurated17.jpg";
+import centurycurated18 from "/src/assets/images/centurycuratedimages/centurycurated18.jpg";
+import centurycurated19 from "/src/assets/images/centurycuratedimages/centurycurated19.jpg";
 
 export const projects = [
     {
@@ -52,6 +71,11 @@ export const projects = [
         title: "Bottle-to-Battery: Emergency Charging Station using Reverse Vending Machine System",
         description: "Our Capstone Project Bottle-to-Battery is a solar-powered charging station that exchanges emergency electricity for mobile devices in return for recycled plastic bottles. This dual-purpose solution tackles both plastic waste and energy access in one sustainable system.",
         tech: ["HTML", "CSS", "Laravel", "Bootstrap", "MySQL", "Firebase", "Arduino"],
+    },
+    {
+        title: "Century Curated: Mid-century Modern Furniture E-commerce Website",
+        description: "Century Curated is an e-commerce website specializing in mid-century modern furniture made with WordPress and WooCommerce. The platform offers a seamless shopping experience with features like product browsing, detailed descriptions, customer reviews, and secure checkout.",
+        tech: ["WordPress", "WooCommerce", "Astra", "Elementor"],
     }
 ];
 
@@ -94,6 +118,27 @@ export const projectImages = {
         btb12,
         btb13,
         btb14
+    ],
+    "Century Curated: Mid-century Modern Furniture E-commerce Website": [
+        centurycurated1,
+        centurycurated2,
+        centurycurated3,
+        centurycurated4,
+        centurycurated5,
+        centurycurated6,
+        centurycurated7,
+        centurycurated8,
+        centurycurated9,
+        centurycurated10,
+        centurycurated11,
+        centurycurated12,
+        centurycurated13,
+        centurycurated14,
+        centurycurated15,
+        centurycurated16,
+        centurycurated17,
+        centurycurated18,
+        centurycurated19,
     ]
 };
 
